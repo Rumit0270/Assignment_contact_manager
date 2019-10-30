2155e295369836506d621d1370fd3d15122dc9cb
Rumit Tandukar <rumit0270@gmail.com>
2019-10-30 14:49:35 +0545

    [ADD] define passport middleware for verifying JWT

---
+++ middlewares/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const { User } = require('../models').models;
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('Authorization'),
+    secretOrKey: process.env.JWT_TOKEN_SECRET
+};
+
+// create a new jwt strategy with options and verify callback
+const jwtAuth = new JwtStrategy(jwtOptions, function(payload, done) {
+
+    const userId = payload.sub;
+    console.log(userId)
+    User.findByPk(userId).then(user => {
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    }, err => {
+        return done(err, false);
+    });
+});
+
+passport.use(jwtAuth);
+
+module.exports = passport;
--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,25 @@
 const { Router } = require('express');
 const { verfiyFBLogin } = require('../middlewares/auth');
 const { generateJWTTOken } = require('../utils');
+const passport = require('../middlewares/passport');
 
 const router = Router();
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 router.get('/login', verfiyFBLogin, (req, res) => {
 
     const user = req.user;
-    console.log(user.id);
-    const token = generateJWTTOken(user.id);
+    const token = generateJWTTOken(user);
     res.status(200).json({
         token
     });
 });
+
+// test endpoint
+router.get('/validateToken', requireAuth, (req, res) => {
+    res.status(200).json({
+        user: req.user.dataValues
+    })
+})
 
 module.exports = router;
